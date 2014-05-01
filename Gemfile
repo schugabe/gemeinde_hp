afc2714c9d78e1fb7ff5bebf41a0b6be406af426
Johannes Kasberger <schugabe@gmx.at>
2014-05-01 11:25:45 +0200

    env file

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'dotenv-rails', :groups => [:production, :development, :test]
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1'
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :chruby_ruby, 'ruby-2.1'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .env}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -34,8 +34,6 @@ set :linked_files, %w{config/database.yml}
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
-after "deploy:update_code", "db:symlink"
 
 namespace :deploy do
 
