f01eed04ad6ca28e090d45ead19f0769d4da9904
Johannes Kasberger <schugabe@gmx.at>
2014-05-01 12:48:39 +0200

    unicorn config file

--- Gemfile
+++ Gemfile
@@ -71,14 +71,5 @@ group :development do
   gem 'capistrano3-unicorn'
 end
 
-# Use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Use Capistrano for deployment
-# gem 'capistrano', group: :development
-
 # Use debugger
 # gem 'debugger', group: [:development, :test]
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,38 +5,12 @@ set :application, 'evang-thening.at'
 set :repo_url, 'https://github.com/schugabe/gemeinde_hp.git'
 
 set :chruby_ruby, 'ruby-2.1'
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :linked_files, %w{config/database.yml .env config/unicorn.rb}
+set :keep_releases, 5
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-set :linked_files, %w{config/database.yml .env}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :unicorn_config_path, "#{release_path}/config/unicorn.rb"
 
 namespace :deploy do
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
