fa3a553390c6d68a0e694fb5fc96bd5d01f6e8eb
Johannes Kasberger <schugabe@gmx.at>
2014-10-28 13:38:40 +0100

    updated pdf.js

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -3,4 +3,6 @@ class Magazine < ActiveRecord::Base
   include Authority::Abilities
   has_attached_file :pdf, styles: {thumbnail: ["1000x1000>", :png]}
   validates_attachment_content_type :pdf, content_type: "application/pdf"
+  
+  default_scope { order("year desc,issue desc") }
 end
