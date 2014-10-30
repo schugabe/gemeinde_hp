5d44f2d662b617fc841f1514ef4dd6a0b87127af
Johannes Kasberger <schugabe@gmx.at>
2014-10-30 08:49:11 +0100

    js moved out of head, small fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @events = Event.upcoming.limit(5)
-    @magazine = Magazine.last
+    @magazine = Magazine.first
     @podcasts = Attachment.where("upload_content_type like 'audio%'").order("created_at desc").limit(3)
   end
 end
